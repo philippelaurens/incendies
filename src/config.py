aa7a2e896eb3b957c83fe583b36a363ee2793398
bruno-coulet <bruno.coulet@laplateforme.io>
2026-09-11 15:41:03 +0200

    rename eda notebook to bdiff

--- src/config.py
+++ src/config.py
@@ -9,3 +9,5 @@ DATA_PROCESSED_DIR = ROOT_DIR / "data" / "data_processed"
 
 GEO_DATA_RAW_DIR = ROOT_DIR / "data" / "data_geo_raw"
 GEO_DATA_CLEAN_DIR = ROOT_DIR / "data" / "data_geo_clean"
+
+FIGURES_DIR = ROOT_DIR / "figures"
