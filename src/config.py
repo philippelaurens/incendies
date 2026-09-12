88eaf0f548e99d7455cb919af1bfe8aea396e81f
Philippe Laurens <philippe.laurens@laplateforme.io>
2026-09-12 19:26:59 +0200

    first commit on branch postgres

---
+++ src/config.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parent.parent
+
+BDIFF_RAW_DIR = ROOT_DIR / "data" / "bdiff_data_raw"
+BDIFF_CLEAN_DIR = ROOT_DIR / "data" / "bdiff_data_clean"
+
+GEO_DATA_RAW_DIR = ROOT_DIR / "data" / "geo_data_raw"
+GEO_DATA_CLEAN_DIR = ROOT_DIR / "data" / "geo_data_clean"
+
+DATA_PROCESSED_DIR = ROOT_DIR / "data" / "data_processed"
+
+FIGURES_DIR = ROOT_DIR / "figures"
+
