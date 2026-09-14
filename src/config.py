ab5bade09547b7771a219c9a9821f44d83d20406
bruno-coulet <bruno.coulet@laplateforme.io>
2026-09-14 14:35:20 +0200

    Feat: lightgm et xgboost

--- src/config.py
+++ src/config.py
@@ -2,13 +2,15 @@ from pathlib import Path
 
 ROOT_DIR = Path(__file__).parent.parent
 
-BDIFF_RAW_DIR = ROOT_DIR / "data" / "bdiff_data_raw"
-BDIFF_CLEAN_DIR = ROOT_DIR / "data" / "bdiff_data_clean"
+DATA = ROOT_DIR / "data"
 
-GEO_DATA_RAW_DIR = ROOT_DIR / "data" / "geo_data_raw"
-GEO_DATA_CLEAN_DIR = ROOT_DIR / "data" / "geo_data_clean"
+BDIFF_RAW_DIR = DATA / "bdiff_data_raw"
+BDIFF_CLEAN_DIR = DATA / "bdiff_data_clean"
 
-DATA_PROCESSED_DIR = ROOT_DIR / "data" / "data_processed"
+GEO_DATA_RAW_DIR = DATA / "geo_data_raw"
+GEO_DATA_CLEAN_DIR = DATA / "geo_data_clean"
+
+DATA_PROCESSED_DIR = DATA / "data_processed"
 
 FIGURES_DIR = ROOT_DIR / "figures"
 
