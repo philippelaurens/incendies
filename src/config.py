4ef459cf6cac50ab933614949bd4917928df32f1
Bruno Coulet <bruno.coulet@laplateforme.io>
2026-09-15 02:56:32 +0200

    Chore: refacto feature_engineering et eda, modeling

--- src/config.py
+++ src/config.py
@@ -2,15 +2,17 @@ from pathlib import Path
 
 ROOT_DIR = Path(__file__).parent.parent
 
-DATA = ROOT_DIR / "data"
+DATA_DIR = ROOT_DIR / "data"
 
-BDIFF_RAW_DIR = DATA / "bdiff_data_raw"
-BDIFF_CLEAN_DIR = DATA / "bdiff_data_clean"
+BDIFF_RAW_DIR = DATA_DIR / "bdiff_data_raw"
+BDIFF_CLEAN_DIR = DATA_DIR / "bdiff_data_clean"
 
-GEO_DATA_RAW_DIR = DATA / "geo_data_raw"
-GEO_DATA_CLEAN_DIR = DATA / "geo_data_clean"
+GEO_DATA_RAW_DIR = DATA_DIR / "geo_data_raw"
+GEO_DATA_CLEAN_DIR = DATA_DIR / "geo_data_clean"
 
-DATA_PROCESSED_DIR = DATA / "data_processed"
+SPATIO_TEMP_DATA_DIR = DATA_DIR / "spatio_temp"
+
+DATA_PROCESSED_DIR = DATA_DIR / "data_processed"
 
 FIGURES_DIR = ROOT_DIR / "figures"
 
