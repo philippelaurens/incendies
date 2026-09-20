edafa6fe38114cfe8a639eba0a902f4cbaf3d5ca
Philippe Laurens <philippe.laurens@laplateforme.io>
2026-09-20 02:09:35 +0200

    first fix of score calcul

--- app/streamlit/utils/scores.py
+++ app/streamlit/utils/scores.py
@@ -7,7 +7,9 @@ def calculer_score_histo(df: pd.DataFrame) -> pd.Series:
     term_90j = np.log1p(df["nb_incendies_90j"])
     term_365j = np.log1p(df["nb_incendies_365j"])
     term_surf_5a = np.log1p(df["surface_totale_5a"])
-    y_t_365 = df["y_t_365"] if "y_t_365" in df.columns else 0
+
+    y_t_365 = (df["nb_incendies_365j"] > 0).astype(int)
+
     return (
         0.25 * term_30j
         + 0.20 * term_90j
@@ -21,13 +23,16 @@ def calculer_score_topo(df: pd.DataFrame) -> pd.Series:
     term_10k = np.log1p(df["buffer_10km"])
     term_20k = np.log1p(df["buffer_20km"])
     term_50k = np.log1p(df["buffer_50km"])
-    terme_gravitaire = (
-        df["terme_gravitaire_50km"] if "terme_gravitaire_50km" in df.columns else 0
-    )
+
+    # Somme de :
+    # - la surface_parcourue - surf(j) - des feux de toutes les communes cj dans un rayon de 50 km de la commune c
+    # - divisée par
+    # - la distance² séparant cj de c
+    surface = 0
+
     return (
-        0.25 * term_10k
+        0.50 * term_10k
         + 0.25 * term_20k
         + 0.25 * term_50k
-        + 0.25 * terme_gravitaire
-        + 1e-6
+        + 0.00 * surface
     )
